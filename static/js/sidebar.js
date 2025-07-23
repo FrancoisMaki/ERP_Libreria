5b52b0d66db51cd08c6eec5d7f47195ec75a650f
FrancoisMaki <francoismakijess@gmail.com>
2025-07-24 00:19:20 +0200

    add submenus to the sidebar

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,8 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const sidebar = document.getElementById('sidebar');
+
     const toggleBtn = document.getElementById('toggleSidebar');
     const mantenimientoBtn = document.getElementById('mantenimientoBtn');
     const mantenimientoDropdown = document.getElementById('mantenimientoDropdown');
+    
+    const ventasBtn = document.getElementById('ventasBtn');
+    const ventasDropdown = document.getElementById('ventasDropdown');
+
+    const comprasBtn = document.getElementById('comprasBtn');
+    const comprasDropdown = document.getElementById('comprasDropdown');
+
+    const almacenBtn = document.getElementById('almacenBtn');
+    const almacenDropdown = document.getElementById('almacenDropdown');
+
+    const estadisticasBtn = document.getElementById('estadisticasBtn');
+    const estadisticasDropdown = document.getElementById('estadisticasDropdown');
+
     const flechaDropdown = document.getElementById('flechaDropdown');
 
     // Contracción/expansión sidebar
@@ -42,4 +56,97 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 300); // espera la animación de expandir
         }
     });
+
+    // Expansión submenú de ventas
+    ventasBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (sidebar.classList.contains('expanded')) {
+            const abierto = !ventasDropdown.classList.contains('hide');
+            if (abierto) {
+                ventasDropdown.classList.add('hide');
+                ventasBtn.classList.remove('ventas-open');
+            } else {
+                ventasDropdown.classList.remove('hide');
+                ventasBtn.classList.add('ventas-open');
+            }
+        } else {
+            sidebar.classList.remove('contraido');
+            sidebar.classList.add('expanded');
+            setTimeout(() => {
+                ventasDropdown.classList.remove('hide');
+                ventasBtn.classList.add('ventas-open');
+            }, 300);
+        }
+    });
+
+    // Expansión submenú de compras
+    comprasBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (sidebar.classList.contains('expanded')) {
+            const abierto = !comprasDropdown.classList.contains('hide');
+            if (abierto) {
+                comprasDropdown.classList.add('hide');
+                comprasBtn.classList.remove('compras-open');
+            } else {
+                comprasDropdown.classList.remove('hide');
+                comprasBtn.classList.add('compras-open');
+            }
+        } else {
+            sidebar.classList.remove('contraido');
+            sidebar.classList.add('expanded');
+            setTimeout(() => {
+                comprasDropdown.classList.remove('hide');
+                comprasBtn.classList.add('compras-open');
+            }, 300);
+        }
+    });
+
+    // Expansión submenú de almacen
+    almacenBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (sidebar.classList.contains('expanded')) {
+            const abierto = !almacenDropdown.classList.contains('hide');
+            if (abierto) {
+                almacenDropdown.classList.add('hide');
+                almacenBtn.classList.remove('almacen-open');
+            } else {
+                almacenDropdown.classList.remove('hide');
+                almacenBtn.classList.add('almacen-open');
+            }
+        } else {
+            sidebar.classList.remove('contraido');
+            sidebar.classList.add('expanded');
+            setTimeout(() => {
+                almacenDropdown.classList.remove('hide');
+                almacenBtn.classList.add('almacen-open');
+            }, 300);
+        }
+    });
+
+    // Expansión submenú de estadísticas
+    estadisticasBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        if (sidebar.classList.contains('expanded')) {
+            const abierto = !estadisticasDropdown.classList.contains('hide');
+            if (abierto) {
+                estadisticasDropdown.classList.add('hide');
+                estadisticasBtn.classList.remove('estadisticas-open');
+            } else {
+                estadisticasDropdown.classList.remove('hide');
+                estadisticasBtn.classList.add('estadisticas-open');
+            }
+        } else {
+            sidebar.classList.remove('contraido');
+            sidebar.classList.add('expanded');
+            setTimeout(() => {
+                estadisticasDropdown.classList.remove('hide');
+                estadisticasBtn.classList.add('estadisticas-open');
+            }, 300);
+        }
+    });
+    
 });
